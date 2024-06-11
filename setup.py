34efd44f2e22fb27ecc3b9f02b44e5238cf2a95f
John <john@eaxmple.com>
2024-06-11 22:45:33 +0200

    update1106

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='swollen',
+    version='0.0.1',
+    author='Fedir Tymoshchuk',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ftymoshchuk',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
